a3396174f59f22c17fc4f8123b9ddfc7d22d20c8
sangareadama <70858532+sangareadama@users.noreply.github.com>
2022-08-11 20:24:35 +0000

    permit all de admin

--- src/main/java/com/sang/school/schoolManagement/config/SecurityConfiguration.java
+++ src/main/java/com/sang/school/schoolManagement/config/SecurityConfiguration.java
@@ -56,7 +56,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				  		).permitAll()
 				   .antMatchers("/admin/user/**").permitAll()
 		  .antMatchers("/management/**").hasAnyRole("ELEVE","ADMIN")
-		  .antMatchers("/admin/**").hasRole("ADMIN")
+		  .antMatchers("/admin/**")..permitAll()
 		  .antMatchers("/enseignant/**").hasRole("ENSEIGNANT")
 		  
 		  .anyRequest().authenticated()
